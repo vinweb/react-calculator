f563f6fcab87908f55072f14f47102738e3cac15
Vincze János <vinczit@gmail.com>
2020-06-06 10:08:44 +0200

    Updates

---
+++ precache-manifest.6d9bca152d88493f433d28c020ccc927.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "002a89695bcacbd8fa18c49fe37efd86",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "047ede9ccaf11a9c943f",
+    "url": "/react-calculator/static/css/main.06252b00.chunk.css"
+  },
+  {
+    "revision": "7c0c402d45c3872e4991",
+    "url": "/react-calculator/static/js/2.7c094157.chunk.js"
+  },
+  {
+    "revision": "01b52e8cfa161811ba4cbd78f485a21f",
+    "url": "/react-calculator/static/js/2.7c094157.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "047ede9ccaf11a9c943f",
+    "url": "/react-calculator/static/js/main.39ac6daa.chunk.js"
+  },
+  {
+    "revision": "cbb41c4b3088b1b89848",
+    "url": "/react-calculator/static/js/runtime-main.c495d3f6.js"
+  }
+]);
